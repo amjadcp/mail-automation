e399ee6ab679c5b0e1834ce3fb438704efe81873
amjadcp <amjadcp03@gmail.com>
2021-12-03 11:48:35 +0530

    first

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('calicut673007@gmail.com', PASSWORD)
+server.login('FROM GMAIL', 'PASSWORD')
 server.sendmail(
-    'calicut673007@gmail.com',
-    'calicut673007@gmail.com',
-    'This is a python script  generated mail'
+    'FROM GMAIL',
+    'TO GMAIL',
+    'MESSAGE'
 )
